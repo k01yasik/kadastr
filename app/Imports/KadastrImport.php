c9630a4862cbfa673c7aa13588d811c2553e354b
k01yasik <bzdykin@mail.ru>
2020-06-02 16:44:53 +0300

    Final version

--- app/Imports/KadastrImport.php
+++ app/Imports/KadastrImport.php
@@ -20,20 +20,64 @@ class KadastrImport implements ToCollection
 
         foreach ($rows as $row)
         {
-            $response = $client->request('GET', 'http://rosreestr.ru/api/online/address/fir_objects', [
-                'query' => [
-                    'macroRegionId' => '158000000000',
-                    'regionId' => '158229000000',
-                    'street' => strval($row[7]),
-                    'house' => strval($row[9]),
-                    'apartment' => strval($row[10])
-                ],
-                'delay' => 500
-            ]);
+            if (strval($row[5]) === 'Приозерный') {
+                $response = $client->request('GET', 'http://rosreestr.ru/api/online/address/fir_objects', [
+                    'query' => [
+                        'macroRegionId' => '158000000000',
+                        'regionId' => '158229000000',
+                        'settlementId' => '158229840034',
+                        'house' => strval($row[7]),
+                        'apartment' => strval($row[8])
+                    ],
+                    'delay' => 800
+                ]);
+            } else if (strval($row[5]) === 'Крулихино') {
+                $response = $client->request('GET', 'http://rosreestr.ru/api/online/address/fir_objects', [
+                    'query' => [
+                        'macroRegionId' => '158000000000',
+                        'regionId' => '158229000000',
+                        'settlementId' => '158229805008',
+                        'house' => strval($row[7]),
+                        'apartment' => strval($row[8])
+                    ],
+                    'delay' => 800
+                ]);
+            } else if (strval($row[5]) === 'Духново') {
+                $response = $client->request('GET', 'http://rosreestr.ru/api/online/address/fir_objects', [
+                    'query' => [
+                        'macroRegionId' => '158000000000',
+                        'regionId' => '158229000000',
+                        'settlementId' => '158229815001',
+                        'house' => strval($row[7]),
+                        'apartment' => strval($row[8])
+                    ],
+                    'delay' => 800
+                ]);
+            } else if (strval($row[5]) === 'Бездедово') {
+                $response = $client->request('GET', 'http://rosreestr.ru/api/online/address/fir_objects', [
+                    'query' => [
+                        'macroRegionId' => '158000000000',
+                        'regionId' => '158229000000',
+                        'settlementId' => '158229835002',
+                        'house' => strval($row[7]),
+                        'apartment' => strval($row[8])
+                    ],
+                    'delay' => 800
+                ]);
+            } else {
+                $response = $client->request('GET', 'http://rosreestr.ru/api/online/address/fir_objects', [
+                    'query' => [
+                        'macroRegionId' => '158000000000',
+                        'regionId' => '158229000000',
+                        'street' => strval($row[7]),
+                        'house' => strval($row[9]),
+                        'apartment' => strval($row[10])
+                    ],
+                    'delay' => 800
+                ]);
+            }            
 
             $r = json_decode($response->getBody()->getContents(), true);
-
-            $result = array();
 
             if (is_array($r)) {
                 $result = $r[0];
